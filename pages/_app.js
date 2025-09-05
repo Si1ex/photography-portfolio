9d7de4161b17ce8046f272f912a19edb43292b33
Si1eX <dankurhinen@gmail.com>
2025-09-05 10:25:42 +0300

    app.js

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import Script from "next/script";
+import "@/styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-E7JN7V32K1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-E7JN7V32K1');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
