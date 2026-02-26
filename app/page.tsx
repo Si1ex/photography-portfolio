66c9ec72e6455c30b76e936043f1a936cb036dbe
Daniel Kurhinen <35920803+Si1ex@users.noreply.github.com>
2026-02-26 15:38:27 +0200

    Update background image URL in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function HomePage() {
         <div
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: `url('https://res.cloudinary.com/dhjbxoyfw/image/upload/v1757102069/IMG_2734.jpg')`,
+            backgroundImage: `url('https://res.cloudinary.com/dhjbxoyfw/image/upload/v1772111519/20260120-DSC05810.jpg')`,
           }}
         >
           <div className="absolute inset-0 bg-black/40"></div>
